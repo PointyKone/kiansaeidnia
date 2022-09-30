bc6bd4319325dbf119590294d91da2ac3487ec18
Kian Saeidnia <ksaeidnia06@gmail.com>
2022-09-30 19:27:52 +1000

    friends

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,7 +8,7 @@ const about = () => {
 				Hey there! My name is Kian and I am from Australia. I love to code things that have function. My favourite languages are Typescript and Rust.
 			</p>
 			<div className="w-80 h-80 mt-10">
-				<h1 className="text-2xl font-semibold mb-2">Helpers:</h1>
+				<h1 className="text-2xl font-semibold mb-2">Friends:</h1>
 				<img src="/Dog.png" alt="" />
 			</div>
 			
