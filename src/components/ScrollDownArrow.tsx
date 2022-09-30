0e17c2dfb7b454ad74d2845ada58bbde83b42a35
Kian Saeidnia <ksaeidnia06@gmail.com>
2022-09-30 16:06:11 +1000

    fixed styling issues

--- src/components/ScrollDownArrow.tsx
+++ src/components/ScrollDownArrow.tsx
@@ -1,7 +1,7 @@
 const ScrollDownArrow = () => {
   return (
     <div className="w-full animate-bounce flex justify-center ">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
         </svg>
     </div>
