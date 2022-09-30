2c2370c2b8825dd9c312bbea1bcb6547c93b8744
Kian Saeidnia <ksaeidnia06@gmail.com>
2022-09-30 19:25:17 +1000

    dog

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image"
+
 const about = () => {
-  return (
-    <div className="text-center text-gray-600 font-semibold mt-20 text-xl">About</div>
-  )
+  	return (
+		<div>
+			<h1 className="text-7xl font-bold">About Me</h1>
+			<p className="mt-5">
+				Hey there! My name is Kian and I am from Australia. I love to code things that have function. My favourite languages are Typescript and Rust.
+			</p>
+			<div className="w-80 h-80 mt-10">
+				<h1 className="text-2xl font-semibold mb-2">Helpers:</h1>
+				<img src="/Dog.png" alt="" />
+			</div>
+			
+		</div>
+  	)
 }
 
 export default about
