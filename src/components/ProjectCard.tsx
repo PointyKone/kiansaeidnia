38652ff0be908a158e42b3ff173d6ae08c38c002
Kian Saeidnia <ksaeidnia06@gmail.com>
2022-10-19 18:23:08 +1000

    glow effect around pinned repos

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
+import styles from "../styles/card.module.css"
 
 const ProjectCard: FC<{
     url: string;
@@ -7,9 +8,23 @@ const ProjectCard: FC<{
     language: string;
     languageColor: string;
 }> = ({ url, repo, description, language, languageColor }) => {
-    return (
-        <a href={url} target="_blank" rel="noreferrer">
-            <div className="flex flex-col p-4 transition-transform duration-300 border-2 rounded-lg h-40 border-t-pink dark:bg-[#1c1c1c] bg-[#d47fff] hover:bg-[hsl(280,100%,70%)] text-white place-content-evenly transform hover:scale-[104%]">
+
+    const card = useRef<HTMLDivElement>(null);
+
+    const handleOnMouseMove = (e: MouseEvent) => {
+        const { currentTarget } = e
+
+        const rect = (currentTarget as HTMLDivElement).getBoundingClientRect(), x = e.clientX - rect.left, y = e.clientY - rect.top;
+        (currentTarget as HTMLDivElement).style.setProperty("--mouse-x", `${x}px`);
+        (currentTarget as HTMLDivElement).style.setProperty("--mouse-y", `${y}px`);
+    }
+
+    useEffect(() => {
+        if (card !== null) {
+            card.current!.onmousemove = e => handleOnMouseMove(e);
+        }
+    }, [card])
+     {/* <div className="flex flex-col p-4 transition-transform duration-300 border-2 rounded-lg h-40 border-t-pink dark:bg-[#1c1c1c] bg-[#d47fff] hover:bg-[hsl(280,100%,70%)] text-white place-content-evenly transform hover:scale-[104%]">
                 <div className="flex flex-col gap-1">
                     <p className="text-xl font-medium medium-text">{repo}</p>
                     <p className="text-sm">{description}</p>
@@ -20,6 +35,25 @@ const ProjectCard: FC<{
                     <span style={{ color: languageColor }}>⬤</span>
                         {language}
                     </span>
+                </div>
+            </div> */}
+
+    return (
+        <a href={url} target="_blank" rel="noreferrer">
+            <div ref={card} className={`${styles.card} card h-[120px] w-[282px] rounded-[10px] cursor-pointer relative flex justify-center align-middle`}>
+                <div className={`${styles.cardBorder}`}></div>
+                <div className={`${styles.cardContent} card-content p-3`}>
+                    <div className="flex flex-col gap-1">
+                        <p className="text-xl font-medium medium-text">{repo}</p>
+                        <p className="text-sm">{description}</p>
+                    </div>
+    
+                    <div className="flex flex-col pt-4 text-sm gap-0.5">
+                        <span className="flex items-center gap-1.5">
+                        <span style={{ color: languageColor }}>⬤</span>
+                            {language}
+                        </span>
+                    </div>
                 </div>
             </div>
         </a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,17 +15,17 @@ const Home: NextPage<{
 			</Head>
 			<Hero pinnedRepos={pinnedRepos}/>
             <h3 className="text-2xl my-3 font-semibold">Here are some of my favourite projects:</h3>
-			<div className="grid grid-cols-1 gap-4 auto-cols-max sm:grid-cols-3 sm:gap-3">
-            {pinnedRepos.map((project) => (
-                <ProjectCard
-                key={project.repo}
-                repo={project.repo}
-                url={`https://github.com/${project.owner}/${project.repo}`}
-                description={project.description}
-                language={project.language}
-                languageColor={project.languageColor}
-                />
-            ))}
+			<div className="flex flex-wrap max-w-[916px] gap-[8px] justify-center">
+                {pinnedRepos.map((project) => (
+                    <ProjectCard
+                    key={project.repo}
+                    repo={project.repo}
+                    url={`https://github.com/${project.owner}/${project.repo}`}
+                    description={project.description}
+                    language={project.language}
+                    languageColor={project.languageColor}
+                    />
+                ))}
             </div>
 		</div>
   )
